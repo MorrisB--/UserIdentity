f52f238fa9fa50a2cc76ab9472536c2834fc66ce
MorrisB-- <morris.ballenger@yahoo.com>
2016-11-15 20:15:00 -0600

    Created test method Secret and Overt

---
+++ UserIdentity/UserIdentity/Controllers/SecretController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UserIdentity.Controllers
+{
+    public class SecretController : Controller
+    {
+        // GET: Secret
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Secret() {
+            return "Secret";
+        }
+
+        public string Overt() {
+            return "Not a secret";
+        }
+    }
+}
